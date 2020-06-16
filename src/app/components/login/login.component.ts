6890708a56e315cffaa5b7003629b97ff3887c5e
Ravi Deep <raviit2015>
2020-06-16 23:01:19 +0530

    first commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  exavaultExavault(){
+    window.location.href='';
+  }
+  
+  mediHostLogin(){
+    window.location.href='';
+  }
+  webshuttleLogin(){
+    window.location.href='';
+  }
+
+}
